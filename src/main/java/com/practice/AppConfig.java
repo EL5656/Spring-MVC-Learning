7e7f90a3a7565957296d9c686116542482ec8ac3
elaine460 <102460032+elaine460@users.noreply.github.com>
2024-09-29 23:54:53 +0800

    InternalResourceViewResolver

--- src/main/java/com/practice/AppConfig.java
+++ src/main/java/com/practice/AppConfig.java
@@ -1,10 +1,18 @@
 package com.practice;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan("com.practice")
 public class AppConfig {
-
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
 }
